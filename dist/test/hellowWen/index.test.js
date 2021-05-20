80ae5c379581687f57381f4466e37ff41d9047bf
xuhang <929175050@qq.com>
2021-05-20 11:37:22 +0800

    添加jest

---
+++ dist/test/hellowWen/index.test.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const _src_1 = require("@src");
+test('helloWen', () => {
+    expect(_src_1.helloWen()).toBeCalled();
+});
